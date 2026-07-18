13165425103af7d324127ea155dd81864fb84648
TUNGLOL <nguyenduongminhtung2167@gmail.com>
2026-07-18 11:27:13 +0700

    update giao diện addCardSerivce

--- PBMS-fe/src/config.ts
+++ PBMS-fe/src/config.ts
@@ -2,4 +2,4 @@
  * Cấu hình chung cho hệ thống Frontend.
  * Bạn có thể thay đổi API Key ở đây hoặc thiết lập biến môi trường VITE_GEMINI_API_KEY.
  */
-export const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY || "?";
+export const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY || "AQ.Ab8RN6LulMxxsAB0HiTXVWfM9VPy1rgGZeew0Ts1ULXqPGejtw";
